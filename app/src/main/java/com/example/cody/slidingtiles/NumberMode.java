667091275585a91797aa737cc6ce99c3d07d0f61
amarjit <amarjit2@pdx.edu>
2018-02-14 14:28:19 -0800

    I have added timer in number mode with play and pause button

--- app/src/main/java/com/example/cody/slidingtiles/NumberMode.java
+++ app/src/main/java/com/example/cody/slidingtiles/NumberMode.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.SystemClock;
 
 public class NumberMode extends AppCompatActivity {
 
@@ -15,6 +18,17 @@ public class NumberMode extends AppCompatActivity {
     float yTileDistance = 0;
     int emptyTileRowIndex;
     int emptyTileColIndex;
+
+    // Timer variables
+    private Button startButton;
+    private Button pauseButton;
+    private TextView timerValue;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
 
     //UI Elements
     Button emptyTileButton;
@@ -28,6 +42,23 @@ public class NumberMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_mode);
+        // Timer implementation
+        timerValue = (TextView) findViewById(R.id.timerValue);
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
 
         //Create a 2-D array of the board
         tileMatrix = boardGen.generateNumberModeBoard();
@@ -42,6 +73,22 @@ public class NumberMode extends AppCompatActivity {
 
 
     }
+
+    // Timer code
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"
+                            + String.format("%02d", secs) + ":"
+                            + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
 
     // Takes a 2-d array and maps it to UI elements
     protected void displayBoardMatrixUI(final GridLayout board) {
